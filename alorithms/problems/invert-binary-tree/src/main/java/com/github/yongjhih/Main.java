9cb12ba8863f361746b2f8bf20b3b529ef1c03d7
Andrew Chen <yongjhih@gmail.com>
2015-06-13 02:04:26 +0800

    Add algorithms/problems/invert-binary-tree

---
+++ alorithms/problems/invert-binary-tree/src/main/java/com/github/yongjhih/Main.java
@@ -0,0 +1,36 @@
+package com.github.yongjhih;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        root = new TreeNode();
+        root.val = 4;
+        root.left = new TreeNode();
+        root.right = new TreeNode();
+
+        root.left.val = 2;
+        root.left.left = new TreeNode();
+        root.left.right = new TreeNode();
+
+        root.right.val = 7;
+        root.right.left = new TreeNode();
+        root.right.right = new TreeNode();
+
+        root.left.left.val = 1;
+        root.left.left.left = new TreeNode();
+        root.left.left.right = new TreeNode();
+
+        root.left.right.val = 3;
+        root.left.right.left = null;
+        root.left.right.right = null;
+
+        root.right.left.val = 6;
+        root.right.left.left = new TreeNode();
+        root.right.left.right = new TreeNode();
+
+        root.right.right.val = 9;
+        root.right.right.left = null;
+        root.right.right.right = null;
+        */
+    }
+}
