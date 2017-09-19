5b727be45787586cd83e90e04add5d1471c309d4
Andrew Chen <yongjhih@gmail.com>
2017-09-19 11:27:16 -0700

    Support long and positive number

--- alorithms/problems/palindrome-number/src/main/java/com/github/yongjhih/Solution4.java
+++ alorithms/problems/palindrome-number/src/main/java/com/github/yongjhih/Solution4.java
@@ -2,17 +2,18 @@ package com.github.yongjhih;
 
 public class Solution4 {
     public boolean isPalindrome(int x) {
+        if (x < 0) return false;
         return reverse(x) == x;
     }
 
     public static int reverse(int x) {
         if ((x / 10) == 0) return x;
-        int r = 0;
+        long r = 0;
         while (x != 0) {
             r *= 10;
             r += x % 10;
             x /= 10;
         }
-        return r;
+        return (int) r;
     }
 }
