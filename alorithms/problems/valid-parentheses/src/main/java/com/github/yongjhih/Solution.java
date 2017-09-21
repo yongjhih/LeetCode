1d164c6565be99186cc419137b3da2b202c4cc6a
Andrew Chen <yongjhih@gmail.com>
2017-09-21 16:20:08 -0700

    Add valid-parentheses

---
+++ alorithms/problems/valid-parentheses/src/main/java/com/github/yongjhih/Solution.java
@@ -0,0 +1,24 @@
+package com.github.yongjhih;
+
+class Solution {
+    public boolean isValid(String s) {
+        if ((s.length() % 2) != 0) return false;
+        Stack<Character> stack = new Stack<Character>();
+
+        for (int i=0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else {
+                if (!stack.isEmpty()) {
+                    if (stack.pop() != c) return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
---
+++ alorithms/problems/valid-parentheses/src/test/java/com/github/yongjhih/MainTests.java
@@ -0,0 +1,12 @@
+package com.github.yongjhih;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class MainTests {
+    @Test
+    public void test() {
+    }
+}
