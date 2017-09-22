e9ce5180040782b0597d398f9badcb3bac6e3a3c
Andrew Chen <yongjhih@gmail.com>
2017-09-22 00:08:02 -0700

    add symmetric-tree

---
+++ alorithms/problems/symmetric-tree/src/main/java/com/github/yongjhih/Solution.java
@@ -0,0 +1,22 @@
+package com.github.yongjhih;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+    public boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null || right == null) return left == right;
+        if (left.val != right.val) return false;
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+}
