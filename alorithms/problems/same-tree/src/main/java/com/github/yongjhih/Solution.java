86a4610140c8a6f78a10a358af998f4596f4765a
Andrew Chen <yongjhih@gmail.com>
2017-09-22 00:07:19 -0700

    Simplize quard condition

--- alorithms/problems/same-tree/src/main/java/com/github/yongjhih/Solution.java
+++ alorithms/problems/same-tree/src/main/java/com/github/yongjhih/Solution.java
@@ -11,9 +11,8 @@ package com.github.yongjhih;
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if (p == null && q == null) return true;
-        if (p == null) return false;
-        if (q == null) return false;
+        if (p == null || q == null) return p == q;
+
         if (p.val != q.val) return false;
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
