6e62274e95e1cd414e1185d301553035b32a6a77
Andrew Chen <yongjhih@gmail.com>
2017-09-22 10:26:39 -0700

    Add add-two-numbers alernative solution

---
+++ alorithms/problems/add-two-numbers/src/main/java/com/github/yongjhih/Solution2.java
@@ -0,0 +1,30 @@
+package com.github.yongjhih;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return addTwoNumbers(l1, l2, 0);
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2, int carry) {
+        if (l1 == null || l2 == null) return null;
+
+        l1.val = l1.val + l2.val + carry;
+
+        carry = (l1.val >= 10) ? 1 : 0;
+        l1.val %= 10;
+        if (carry != 0 || l1.next != null || l2.next != null) {
+            if (l1.next == null) l1.next = new ListNode(0);
+            if (l2.next == null) l2.next = new ListNode(0);
+        }
+        addTwoNumbers(l1.next, l2.next, carry);
+        return l1;
+    }
+}
